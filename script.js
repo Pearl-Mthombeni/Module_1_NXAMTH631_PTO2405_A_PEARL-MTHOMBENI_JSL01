a7055d037bd3aff713862afe31a3fc3ef358b339
Pearl-Mthombeni <pearlmthombeni97@gmail.com>
2024-08-23 11:14:54 +0200

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    let regex = /^pet_[a-zA-Z0-9]+$/; // Expression to validate the pattern "pet_" followed by letters and numbers
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+     // Check if input starts with 'pet_' and followed by alphanumeric characters
+     
+    if (regex.test(input)) {
+        result = "Valid Syntax";
+    } else {
+        result = "Invalid Syntax";
+    }
 
             document.getElementById('result').innerText = result;
 }
